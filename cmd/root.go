704c3dde1a69886df2f37f50e3007c95335a3abf
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2024-06-14 16:51:39 +0700

    update root description

--- cmd/root.go
+++ cmd/root.go
@@ -27,18 +27,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "auto-logwork",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Auto logwork so you don't have to do it",
+	Long: `
+    To auto-logwork, run:
+      configure: to config credentials and the tools 
+      logwork: to start auto logwork
+  `,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
